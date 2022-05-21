069a7c23684dabebb4ea475f9cb5426a13d9c938
zeonga1102 <dlwjddk4587@naver.com>
2022-05-22 03:06:53 +0900

    모든 기능 완성

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from werkzeug.utils import secure_filename
-from bson.objectid import ObjectId
+from flask import Flask, render_template, request, jsonify
+import tensorflow as tf
+import numpy as np
 
 import datetime
 
 from pymongo import MongoClient
 
+model = tf.keras.models.load_model('./static/model.h5')
 
 app = Flask(__name__)
 
@@ -17,17 +18,40 @@ def home():
 def upload():
    file = request.files['file_give']
    fileName = request.form['title_give']
+   extension = file.filename.split('.')[-1]
 
    timestamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
 
-   path = './static/img/' + fileName + '_' + timestamp
+   path = './static/img/' + fileName + '_' + timestamp + extension
    file.save(path)
+
+   doc = {'path': path,
+          'name': fileName}
+
+   db.images.insert_one(doc)
 
    return jsonify({'msg': 'success'})
 
 
-# @app.route('/search')
-# def search():
+@app.route('/api/search', methods=['POST'])
+def search():
+   fileName = request.form['name_give']
+
+   images = list(db.images.find({'name': fileName}, {'_id': False}))
+
+   for image in images:
+      predictImg = tf.keras.preprocessing.image.load_img(image['path'], target_size=(256, 256))
+      input_arr = tf.keras.preprocessing.image.img_to_array(predictImg)
+      input_arr = np.array([input_arr])
+      predictions = model.predict(input_arr)
+      if predictions[0][0] > 0.5:
+         result = '강아지'
+      else:
+         result = '고양이'
+
+      image['pred'] = result
+
+   return jsonify({'images': images})
 
 
 if __name__ == '__main__':
--- static/js/indexJs.js
+++ static/js/indexJs.js
@@ -6,26 +6,49 @@ function loadFile(obj) {
 
     reader.onload = function () {
         $('#uploadImg').attr("src", reader.result);
+        $('#uploadImg').attr("style", 'display: initial');
     }
 }
 
 function uploadFile() {
-    let file = $('#chooseFile')[0].files[0]
-    let title = $('#fileName').val()
-    let form_data = new FormData()
+    let file = $('#chooseFile')[0].files[0];
+    let title = $('#fileName').val();
+    let form_data = new FormData();
 
-    form_data.append("file_give", file)
-    form_data.append("title_give", title)
+    form_data.append("file_give", file);
+    form_data.append("title_give", title);
 
     $.ajax({
         type: "POST",
-        url: "/upload",
+        url: "/api/upload",
         data: form_data,
         cache: false,
         contentType: false,
         processData: false,
         success: function (response) {
             alert(response["msg"])
+        }
+    });
+}
+
+function clickSearchButton() {
+    let name = $('#searchName').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/search',
+        data: {'name_give': name},
+        success: function (response) {
+            let images = response['images'];
+            $('#result').empty();
+
+            for(let i=0; i<images.length; i++) {
+                let path = images[i]['path'];
+                let pred = images[i]['pred'];
+                let temp_html = `<div class="result"><img src="${path}" width="100px"/>
+                                <p>${pred}</p></div>`
+                $('#result').append(temp_html)
+            }
         }
     });
 }
